d2688822a37d730e81ceeee5f614e9adc8040ec1
Azucena <61836749+azucenaGV@users.noreply.github.com>
2020-05-04 18:37:36 -0500

    Add files via upload

---
+++ NumeroMayor.js
@@ -0,0 +1,12 @@
+var inputNum1 = Number (prompt ("Ingresa el numero 1"));
+var inputNum2 = Number (prompt ("Ingresa el numero 2"));
+var inputNum3 = Number (prompt ("Ingresa el numero 3"));
+
+if (inputNum1 > inputNum2 && inputNum1 > inputNum3) {
+document.write ("El numero 1 es mayor por:" + inputNum1);
+    } else  if (inputNum2 > inputNum3){
+     document.write ("El numero 2 es mayor por:" +inputNum2);  
+     } else { 
+    document.write ("El numero 3 es mayor 3 es:" + inputNum3);  
+    }
+    
